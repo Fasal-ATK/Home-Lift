c6ddf6509912f76240e0526ecc1382d151b9126f
Fasal <fasalrahmanatk706@gmail.com>
2025-11-18 20:51:26 +0530

    checking

--- back/HomeLift/bookings/urls.py
+++ back/HomeLift/bookings/urls.py
@@ -11,21 +11,11 @@ from .views import (
 
 urlpatterns = [
 
-    # -------------------------
-    # USER ROUTES
-    # -------------------------
-    # GET   /bookings/        → list user's bookings
-    # POST  /bookings/        → create booking
+
     path("", BookingListCreateView.as_view(), name="booking-list-create"),
 
-    # GET   /bookings/<pk>/   → get specific booking (owner / provider / admin)
-    # DELETE /bookings/<pk>/  → cancel booking (owner / provider / admin)
-    path("<int:pk>/", BookingDetailUpdateView.as_view(), name="booking-detail"),
+    path("details/<int:pk>/", BookingDetailUpdateView.as_view(), name="booking-detail"),
 
-    # -------------------------
-    # BOOKING STATUS UPDATE
-    # -------------------------
-    # PATCH /bookings/<pk>/status/
     path("<int:pk>/status/", BookingStatusUpdateView.as_view(), name="booking-status-update"),
 
     # -------------------------
