a42db5455b4bc1579b2e9a1cbe1af45a212779ea
Fasal <fasalrahmanatk706@gmail.com>
2026-02-06 13:50:03 +0530

    back - admin booking manager created

--- back/HomeLift/admins/urls.py
+++ back/HomeLift/admins/urls.py
@@ -1,6 +1,7 @@
 #admins/urls.py
 from django.urls import path,include
 from .views import AdminLoginView
+from bookings.views import AdminBookingsView, BookingStatusUpdateView
 
 urlpatterns = [
     path('login/', AdminLoginView.as_view() ),
@@ -8,4 +9,6 @@ urlpatterns = [
     path('services/',include('services.urls')),
     path('customers/',include('users.urls')),
     path('providers/',include('providers.urls')),
+    path('bookings/all/', AdminBookingsView.as_view()),
+    path('bookings/<int:pk>/status/', BookingStatusUpdateView.as_view()),
 ]
