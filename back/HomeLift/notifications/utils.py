f46deb804b1a5dcda5eb4dbb5745e60c9a011ec7
Fasal <fasalrahmanatk706@gmail.com>
2026-07-20 17:14:34 +0530

    logger update

--- back/HomeLift/notifications/utils.py
+++ back/HomeLift/notifications/utils.py
@@ -1,12 +1,21 @@
 from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
+import logging
+
+logger = logging.getLogger(__name__)
 
 def send_user_notification(user_id, message):
-    channel_layer = get_channel_layer()
-    async_to_sync(channel_layer.group_send)(
-        f"user_{user_id}",
-        {
-            "type": "send_notification",
-            "message": message,
-        }
-    )
+    try:
+        channel_layer = get_channel_layer()
+        if channel_layer:
+            async_to_sync(channel_layer.group_send)(
+                f"user_{user_id}",
+                {
+                    "type": "send_notification",
+                    "message": message,
+                }
+            )
+        else:
+            logger.warning(f"Could not send notification: Channel layer not configured.")
+    except Exception as e:
+        logger.warning(f"Failed to send real-time notification to user {user_id}: {e}")
